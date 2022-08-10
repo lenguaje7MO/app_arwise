c222f77074b697dd7ef759c495778089b2c7b812
Luis <jorgedesarrollo717@gmail.com>
2022-08-10 05:41:35 -0500

    Agregado permisos de internet

--- src/ui/utils/urls.dart
+++
@@ -1,4 +0,0 @@
-// URLS DE APP
-var contenidoUrl ='https://drive.google.com/drive/folders/1ozLf-HZnFBZLcW64loLEZKypA6irZ5mr?usp=sharing';
-var ejerciciosUrl = 'https://wordwall.net/teacher/12574668/literatura7mo';
-var videosUrl = 'https://edpuzzle.com/content';
